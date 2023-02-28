70f9a5cb09a00d41f8752ea4f06fe30efc7a7a38
AnteausWards <126543635+AnteausWards@users.noreply.github.com>
2023-02-27 23:00:55 -0600

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABotWithMSI.sln -p:DeployOnBuild=true -p:PublishProfile=.\QuestionAnswerCTF-bot-85ed-Web-Deploy.pubxml -p:Password=R8fQfCYXJxy9DBrwNbv6RTd7oDaJixYeW8rqSh1QjWs0nAJvbmtJWRTwkThN
+
